8d05a1621bbbc62a007ca16db455bf9927b24f26
jali-rk <jaliya.r.kumarasiri@gmail.com>
2025-12-28 20:48:53 +0530

    TRY CHANGING V2

--- src/main/resources/db/migration/V2__create_student_code_sequence.sql
+++ src/main/resources/db/migration/V2__create_student_code_sequence.sql
@@ -1,8 +1,7 @@
 -- Create sequence for student registration numbers starting from 1001
-CREATE SEQUENCE student_code_number_seq
+CREATE SEQUENCE IF NOT EXISTS student_code_number_seq
     START WITH 1001
     INCREMENT BY 1
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
-
